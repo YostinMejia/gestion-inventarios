61e8f9247ad51d78afe6046e3bd17e87d1e1b937
yos <ysmaprogramming@gmail.com>
2024-11-12 15:19:43 -0500

    logs eliminados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./layout/header/header.component";
-import { FooterComponent } from "./layout/footer/footer.component";
-import { SupabaseService } from './services/supabase.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'inventarios';
-
-    constructor(private supabaseService:SupabaseService){
-      console.log("Imagenes");
-      
-      supabaseService.getImagen("")
-    }
-
 
 }
